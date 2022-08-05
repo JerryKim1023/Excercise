c237b8a62a1b240c7466645f66f30e6b185047be
김 지용 <74017167+JerryKim1023@users.noreply.github.com>
2022-08-05 11:16:25 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import random
+
+deck = []
+
+# nums과 shapes 정의
+shapes = '♥♣♠◆'
+nums = []
+for i in range(2,11):
+    nums.append(str(i))
+for c in 'JQKA':
+    nums.append(c)
+
+#조커 넣어주기
+deck.append(('Joker', 'black'))
+deck.append(('Joker', 'colored'))
+
+# 덱 만들기
+for shape in shapes:
+    for num in nums:
+        deck.append((shape, num))
+
+# 카드섞기
+random.shuffle(deck)
+
+# 플레이어에게 카드 주기
+player = []
+
+last_card = deck.pop()
+player.append(last_card)
+print("현재 패 >>", player[-1])
+
+
+while True:
+
+    # 플레이어의 차례
+    # 플레이어가 무언가를 한다..
+    if len(deck) == 0:
+        print("하체운동이 끝났습니다!")
+        break
+    else:
+        input = input()
+        last_card = deck.pop()
+        player.append(last_card)
+        print("현재 패 >>", player[-1])
+
+
